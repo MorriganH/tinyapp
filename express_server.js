1c189fbaa89e1c71eb33759e88af71657429a1f0
MorriganH <morriganhennessy@gmail.com>
2023-04-17 19:33:02 -0400

    adds new page with form for user to submit a new url for shortening, and a function to generate random strings of 6 characters for the short urls

--- express_server.js
+++ express_server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const generateRandomString = () => {
+  const characters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  let shortID = '';
+  for (let char = 0; char < 6; char++) {
+    shortID += characters[Math.floor(Math.random() * 62)];
+  }
+};
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.cs',
   '9sm5xK': 'http://www.google.com'
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send("Hello!");
@@ -17,6 +27,10 @@ app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
 
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
@@ -30,6 +44,11 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
