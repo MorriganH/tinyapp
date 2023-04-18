4a3102a02f6bfd6a3bebc4c835094acde64da65e
MorriganH <morriganhennessy@gmail.com>
2023-04-18 17:11:29 -0400

    adds cookie-parser as dependency and can display login status using a cookie

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const PORT = 8080;
 
 const generateRandomString = () => {
@@ -19,22 +20,24 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send("Hello!");
-});
+// app.get('/', (req, res) => {
+//   res.send("Hello!");
+// });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-})
+  const templateVars = { username: req.cookies['username'] };
+  res.render('urls_new', templateVars);
+});
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies['username'] };
   res.render('urls_show', templateVars);
 });
 
@@ -43,13 +46,13 @@ app.get('/u/:id', (req, res) => {
   res.redirect(longURL);
 });
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
+// app.get('/hello', (req, res) => {
+//   res.send('<html><body>Hello <b>World</b></body></html>\n');
+// });
 
 app.post('/urls', (req, res) => {
   const shortID = generateRandomString();
